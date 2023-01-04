ff44c3487a9ec0a0a10166634b22ec8113cc6b84
ninodev30 <ninodm.dev@gmail.com>
2023-01-04 07:10:08 -0300

    feat: create Ad component

--- mobile/src/screens/app/Home/styles.ts
+++ mobile/src/screens/app/Home/styles.ts
@@ -1,10 +1,13 @@
 import { StyleSheet } from "react-native";
 import theme from "@theme/index";
 
+const { COLORS, FONTS, HEIGHT, PADDING } = theme;
+
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: theme.COLORS.BASE.GRAY_700
+        padding: PADDING.SM,
+        backgroundColor: COLORS.BASE.GRAY_700
     },
     title: {
         fontSize: theme.FONTS.SIZES.XL,
