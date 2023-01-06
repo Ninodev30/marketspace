ec8972d1a1db8fc9cfe3913cd56aca05941c8bd0
ninodev30 <ninodm.dev@gmail.com>
2023-01-05 22:19:53 -0300

    fix: exclude the header styles of the ./home/styles.ts

--- mobile/src/screens/app/Home/styles.ts
+++ mobile/src/screens/app/Home/styles.ts
@@ -1,8 +1,7 @@
 import { StyleSheet } from "react-native";
 import theme from "@theme/index";
 
-const { COLORS, FONTS, SCALE, PADDING } = theme;
-const userPhotoSize: number = SCALE.AVERAGE(8);
+const { COLORS, SCALE, PADDING } = theme;
 
 export default StyleSheet.create({
     container: {
@@ -10,34 +9,4 @@ export default StyleSheet.create({
         padding: PADDING.SM,
         backgroundColor: COLORS.BASE.GRAY_600
     },
-    header: {
-        paddingTop: 40,
-        flexDirection: 'row',
-        alignSelf: 'stretch',
-        justifyContent: 'space-between'
-    },
-    userBox: {
-        flexDirection: 'row',
-    },
-    userPhoto: {
-        width: userPhotoSize,
-        height: userPhotoSize,
-        borderRadius: userPhotoSize / 2,
-        marginRight: SCALE.WIDTH(3),
-        borderWidth: SCALE.AVERAGE(0.3),
-        borderColor: COLORS.BASE.GRAY_700
-    },
-    titleBox: {
-        marginRight: SCALE.WIDTH(15),
-    },
-    title: {
-        fontSize: FONTS.SIZES.LG,
-        fontFamily: FONTS.FAMILY.BOLD,
-        color: COLORS.BASE.GRAY_100
-    },
-    subtitle: {
-        fontSize: FONTS.SIZES.MD,
-        fontFamily: FONTS.FAMILY.REGULAR,
-        color: COLORS.BASE.GRAY_100
-    }
 });
